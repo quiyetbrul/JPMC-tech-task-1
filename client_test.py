d8ce3c9e21a5e2dbb66a2df90da7c5d09b7060ad
Quiyet Brul <34882290+quiyetbrul@users.noreply.github.com>
2020-05-14 23:27:54 -0700

    Fixed client. Unit testing added.

--- client_test.py
+++ client_test.py
@@ -1,5 +1,7 @@
 import unittest
 from client import getDataPoint
+# import subprocess
+# import os
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -9,16 +11,23 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), ([quote['stock']], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote ['top_ask']['price'])/2))
+
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), ([quote['stock']], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote ['top_ask']['price'])/2))
 
 
   """ ------------ Add more unit tests ------------ """
-
+  # print("UNIT TEST RESULTS BELOW...")
+  # process2 = subprocess.Popen(['python', 'client_test.py'], cwd=os.getcwd(), preexec_fn=os.setsid)
+  # process2.wait()
 
 
 if __name__ == '__main__':
